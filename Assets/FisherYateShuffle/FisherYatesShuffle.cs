e46dd08608438bc12a2485b8e804a2cbf6e6d7ea
Yongchul-Kwon <40688851+Yongchul-Kwon@users.noreply.github.com>
2018-07-05 19:01:06 +0900

    FisherYateShuffle
    One way of shuffling

---
+++ Assets/FisherYateShuffle/FisherYatesShuffle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+
+public class FisherYatesShuffle
+{				
+	/// <summary>
+	/// An improved version (Durstenfeld) of the Fisher-Yates algorithm with O(n) time complexity
+	/// Permutes the given array
+	/// </summary>
+	/// <param name="array">array to be shuffled</param>
+	public static void Shuffle<T>(T[] array)
+	{
+		Random r = new Random();
+		for (int i = array.Length - 1; i > 0; i--)
+		{
+			int index = r.Next(i);
+			//swap
+			T tmp = array[index];
+			array[index] = array[i];
+			array[i] = tmp;
+		}
+	}
+
+	public static T[] ShuffleArray<T>(T[] array)
+	{
+		Random r = new Random();
+		for (int i = array.Length - 1; i > 0; i--)
+		{
+			int index = r.Next(i);
+			//swap
+			T tmp = array[index];
+			array[index] = array[i];
+			array[i] = tmp;
+		}
+
+		return array;
+	}
+
+	// Seed를 설정해주면 일정하게 랜덤값이 출력된다.
+	public static T[] ShuffleArray<T>(T[] array, int _seed)
+	{
+		Random r = new Random(_seed);
+		for (int i = array.Length - 1; i > 0; i--)
+		{
+			int index = r.Next(i);
+			//swap
+			T tmp = array[index];
+			array[index] = array[i];
+			array[i] = tmp;
+		}
+
+		return array;
+	}
+}
---
+++ Assets/FisherYateShuffle/ShuffleSample.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShuffleSample : MonoBehaviour {
+
+    public GameObject[] ShuffleArray;
+
+    private bool isStart = false;
+
+    private void Start()
+    {
+        InvokeRepeating("ExcuteShuffle", 3f, 3f);
+    }
+
+    private void ExcuteShuffle()
+    {
+        float startTime = Time.realtimeSinceStartup;
+        FisherYatesShuffle.Shuffle<GameObject>(ShuffleArray);
+        double elapsedTime = Time.realtimeSinceStartup - startTime;
+        Debug.LogFormat("Shuffle done! : {0}sec", elapsedTime);
+
+        isStart = true;
+    }
+
+    private void Update()
+    {
+        if (isStart)
+        {
+            for (int i = 0; i < ShuffleArray.Length; i++)
+            {
+                GameObject obj = ShuffleArray[i];
+                obj.transform.localPosition = new Vector3(Mathf.Lerp(obj.transform.position.x, (-7.5f + (2.5f * i)), Time.deltaTime * 4f), 0, 0);
+            }
+        }
+    }
+}
